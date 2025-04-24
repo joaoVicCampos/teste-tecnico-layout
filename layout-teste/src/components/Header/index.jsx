2114a821f09c412daaddd126afe036cd8901d09a
João Victor Campos <ripjv1234@gmail.com>
2025-04-24 17:58:41 -0300

    adiciona icon-car

--- layout-teste/src/components/Header/index.jsx
+++ layout-teste/src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import logo from '../../img/logo.svg'
 import './style.css'
+import { FiSearch, FiUser, FiShoppingCart } from 'react-icons/fi'
 import NavBar from '../NavBar'
 import Dropdown from '../Drowdown'
 import SaleMsg from '../SaleMsg'
@@ -10,14 +11,23 @@ const Header = () => {
             <div className='sale-msg-container'>
                 <SaleMsg />
             </div>
-
             <div className="div-top">
-
                 <div className="div-logo">
                     <img src={logo} alt="logo" className="logo" />
                 </div>
-                <div className="div-input">
-                    <input type="text" placeholder="Digite aqui o que você procura" className="input" />
+                <div className="div-input-wrapper">
+                    <div className='input-wrapper'>
+                        <FiSearch className="input-icon" />
+
+                        <input type="text" placeholder="Digite aqui o que você procura" className="input" />
+                    </div>
+                    <div className="user-profile">
+                        <FiUser className="user-icon" />
+                        <span className="user-greeting">Olá, nome do Cliente!</span>
+                    </div>
+                    <div className="cart-icon">
+                        <FiShoppingCart />
+                    </div>
                 </div>
             </div>
             <div className="navbar-wrapper">
